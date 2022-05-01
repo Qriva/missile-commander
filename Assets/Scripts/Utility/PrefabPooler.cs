e101da8c8961f759f4a255e309691380a09741f4
Maciej Pawlisz <griva199@gmail.com>
2022-05-01 21:15:18 +0200

    Fixed PrefabPooler singleton declaration

--- Assets/Scripts/Utility/PrefabPooler.cs
+++ Assets/Scripts/Utility/PrefabPooler.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Game
 {
-    public class PrefabPooler : Singleton<ProjectilePooler>
+    public class PrefabPooler : Singleton<PrefabPooler>
     {
         // NOTE: Can be converted to property exposed in inspector
         public const int MIN_STACK_CAPACITY = 16;
