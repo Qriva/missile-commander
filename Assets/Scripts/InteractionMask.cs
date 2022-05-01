3889749084db9dd7a10caf934621e81b43c0ba68
Maciej Pawlisz <griva199@gmail.com>
2022-05-01 21:17:22 +0200

    Added InteractionMask

---
+++ Assets/Scripts/InteractionMask.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(menuName="Other/InteractionMask")]
+    public class InteractionMask : ScriptableObject
+    {
+        [SerializeField] private LayerMask mask = -1;
+        
+        public bool Contains(int layer)
+        {
+            return mask == (mask | (1 << layer));
+        }
+    }
+}
